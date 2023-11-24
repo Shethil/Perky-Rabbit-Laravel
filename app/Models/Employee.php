d7a182dd1c8500c95ebeb4a4592b45ee725c615b
Shethil <shithelahammed@gmail.com>
2023-11-24 18:59:23 +0600

    migration & model files are created

---
+++ app/Models/Employee.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function achievements()
+    {
+        return $this->belongsToMany(Achievement::class);
+    }
+}
