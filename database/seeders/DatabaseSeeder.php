fda5c794fff4a29570303315de36bfd111668502
Shethil <shithelahammed@gmail.com>
2023-11-25 18:24:44 +0600

    User Seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             AchievementSeeder::class,
             DepartmentSeeder::class,
             EmployeeSeeder::class,
